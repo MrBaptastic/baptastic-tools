d2506948c446d36eae02869317c2ab65b0291e3d
Adam Brady <adam@boxxen.org>
2015-07-20 20:53:16 +1000

    serve frontend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,22 @@
 
+import path    from "path";
+import express from "express";
+
+const NODE_ENV = process.env.NODE_ENV || "development";
+
+const config = require(path.resolve(__dirname, 'config', NODE_ENV));
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "../frontend")));
+
+app.listen(config.port, config.host, function(err) {
+
+  if(err) {
+    console.error("Error listening:", err);
+    process.exit(1);
+  }
+
+  console.log(`Listening on ${config.host}:${config.port}`);
+
+});
